286d2c9ffc9ff060e1d2576339eae1c07b124105
Eric <eric.j.lee8@gmail.com>
2020-02-08 16:31:23 -0800

    idk

--- myScript.js
+++ myScript.js
@@ -1,4 +1,20 @@
 function displayCoffee() {
-    document.getElementById("demo1").innerHTML = "Hello Dolly!";
-    document.getElementById("demo2").innerHTML = "How are you?";
+    var myObj = {
+        "Blenz":[
+            { "product_name": "Black Coffee", "unit_cost": "$10.47" },
+            { "product_name": "French Vanilla", "unit_cost": "$8.74" },
+            { "product_name": "Green Coffee", "unit_cost": "$20.53" }
+        ],
+        "StarBucks":[
+            { "product_name": "Black Coffee", "unit_cost": "$10.47" },
+            { "product_name": "French Vanilla", "unit_cost": "$8.74" },
+            { "product_name": "Green Coffee", "unit_cost": "$20.53" }
+        ]
+    
+    }
+    document.getElementById("store_name").innerHTML = "Blenz";
+    document.getElementById("coffee_name").innerHTML = myObj.Blenz[1].product_name;
+    document.getElementById("price").innerHTML = myObj.Blenz[1].unit_cost;
   }
+
+  
